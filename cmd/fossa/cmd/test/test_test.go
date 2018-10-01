ef19843dd5b9d5ab93b2fabea3248993ec6e4377
Zach <zcjlavallee@gmail.com>
2018-10-01 11:16:40 -0700

    testing added for test.go

--- cmd/fossa/cmd/test/test_test.go
+++ cmd/fossa/cmd/test/test_test.go
@@ -1,36 +1,120 @@
 package test
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
 	"testing"
 
-	"github.com/fossas/fossa-cli/config"
-	gock "gopkg.in/h2non/gock.v1"
+	"github.com/fossas/fossa-cli/api/fossa"
+	"github.com/stretchr/testify/assert"
+	"github.com/urfave/cli"
 )
 
-func TestPublishWrongResponseStatus(t *testing.T) {
-	defer gock.Off()
+const (
+	timeout = 3
+	orgID   = 3
+)
 
-	url := "http://server.com"
-	gock.New(url).
-		Get("/bar").
-		Reply(200).
-		JSON(map[string]string{"foo": "bar"})
+// taskStatus is a struct that imitates the anonymous struct within fossa.Build
+type taskStatus struct {
+	Status string
+}
 
-	config.BackendEndpoint = url
-	// test.Run(&cli.Context{App:,
-	// flagSet: ,
-	// parentContext: ,})
+func TestRunCustomFetcherCustomProject(t *testing.T) {
+	ts := testServer("custom+"+strconv.Itoa(orgID)+"%2FtestRun$1000", t)
+	flagSet := testFlags("custom", "testRun", ts.URL, 1000)
+	context := cli.NewContext(&cli.App{}, flagSet, &cli.Context{})
+	Run(context)
+	ts.Close()
+}
 
-	// stdout, stderr, err := exec.Run(exec.Cmd{
-	// 	Name: "fossa",
-	// 	Argv: append([]string{"test", "-c="}),
-	// })
-	// fmt.Printf("%+v\n", stdout)
-	// fmt.Printf("%+v", stderr)
-	// if err != nil && stdout == "" {
-	// 	if strings.Contains(stderr, "build constraints exclude all Go files") {
-	// 		fmt.Println(errors.New("bad OS/architecture target"))
-	// 	}
-	// 	fmt.Println("error running fossa test")
-	// }
+func TestRunCustomFetcherGitSSHProject(t *testing.T) {
+	ts := testServer("custom+"+strconv.Itoa(orgID)+"%2Fgit@github.com:fossa%2Ffossa-cli.git$1000", t)
+	flagSet := testFlags("custom", "git@github.com:fossa/fossa-cli.git", ts.URL, 1000)
+	context := cli.NewContext(&cli.App{}, flagSet, &cli.Context{})
+	Run(context)
+	ts.Close()
+}
+
+func TestRunCustomFetcherGitHTTPProject(t *testing.T) {
+	ts := testServer("custom+"+strconv.Itoa(orgID)+"%2Fgithub.com%2Ffossa%2Ffossa-cli$1000", t)
+	flagSet := testFlags("custom", "http://github.com/fossa/fossa-cli.git", ts.URL, 1000)
+	context := cli.NewContext(&cli.App{}, flagSet, &cli.Context{})
+	Run(context)
+	ts.Close()
+}
+
+func TestRunGitFetcherGitSSHProject(t *testing.T) {
+	ts := testServer("git+github.com%2Ffossas%2Ffossa-cli$1000", t)
+	flagSet := testFlags("git", "git@github.com:fossas/fossa-cli.git", ts.URL, 1000)
+	context := cli.NewContext(&cli.App{}, flagSet, &cli.Context{})
+	Run(context)
+	ts.Close()
+}
+
+func TestRunGitFetcherGitHTTPProject(t *testing.T) {
+	ts := testServer("git+github.com%2Ffossas%2Ffossa-cli$1000", t)
+	flagSet := testFlags("git", "http://github.com/fossas/fossa-cli.git", ts.URL, 1000)
+	context := cli.NewContext(&cli.App{}, flagSet, &cli.Context{})
+	Run(context)
+	ts.Close()
+}
+
+func TestRunGitFetcherCustomProject(t *testing.T) {
+	ts := testServer("git+testRun$1000", t)
+	flagSet := testFlags("git", "testRun", ts.URL, 1000)
+	context := cli.NewContext(&cli.App{}, flagSet, &cli.Context{})
+	Run(context)
+	ts.Close()
+}
+
+func testServer(locator string, t *testing.T) *httptest.Server {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Path for OrganizationID
+		if r.URL.EscapedPath() == "/api/cli/organization" {
+			newResp := fossa.Organization{OrganizationID: orgID}
+			request, _ := json.Marshal(newResp)
+			fmt.Fprintf(w, string(request))
+			return
+		}
+
+		// Path for check build
+		if r.URL.EscapedPath() == "/api/cli/"+locator+"/latest_build" {
+			newResp := fossa.Build{Task: taskStatus{Status: "SUCCEEDED"}}
+			request, _ := json.Marshal(newResp)
+			fmt.Fprintf(w, string(request))
+			return
+		}
+
+		// Path for check issues
+		if r.URL.EscapedPath() == "/api/cli/"+locator+"/issues" {
+			newResp := fossa.Build{Task: taskStatus{Status: "SUCCEEDED"}}
+			request, _ := json.Marshal(newResp)
+			fmt.Fprintf(w, string(request))
+			return
+		}
+
+		// Path for incorrect requests
+		assert.Equal(t, "hsd", "sdfsadf")
+		newResp := fossa.Build{Task: taskStatus{Status: "FAILED"}}
+		request, _ := json.Marshal(newResp)
+		fmt.Fprintf(w, string(request))
+	}))
+
+	return ts
+}
+
+func testFlags(fetcher, project, endpoint string, revision int) *flag.FlagSet {
+	flagSet := &flag.FlagSet{}
+	flagSet.Int("timeout", timeout, "")
+	flagSet.Int("revision", revision, "")
+	flagSet.String("fetcher", fetcher, "")
+	flagSet.String("project", project, "")
+	flagSet.String("endpoint", endpoint, "")
+	return flagSet
 }
